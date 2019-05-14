06ddb5f20dac5be09dbe4460800575476ee39a25
JB <JamesKinnahBrown@gmail.com>
2019-05-14 21:05:32 +0100

    update config with jb specific data

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "jblog - James Browns deveopment blog", // <title>
+  shortSiteTitle: "jblog", // <title> ending for posts and pages
+  siteDescription: "The software development blog",
+  siteUrl: "http://jamesbrown.work",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "james brown",
+  authorTwitterAccount: "JBJamesBrownJB",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "james brown",
+  headerSubTitle: "JB chatting about software and stuff",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "jblog - a blog about software",
+  manifestShortName: "jblog", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -28,12 +28,12 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://s.gravatar.com/avatar/d38044ea48c2ce6f7652053a12b52609?s=80",
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/JBJamesBrownJB" },
+    { name: "twitter", url: "https://twitter.com/JBJamesBrownJB" },
+    { name: "facebook", url: "https://www.facebook.com/profile.php?id=717835710" }
   ]
 };
